24fea3f6431e926c3bf11c875c26bed3c415122a
Axel Garcia <axegarci@ucsc.edu>
2021-12-01 01:07:29 -0800

    Add choice in HTML options to open saved articles.

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-browser.runtime.onMessage.addListener(async request => {
-    const settings = await browser.storage.local.get("foreground");
-    browser.tabs.create({url: request.href, active: !!settings.foreground})
+browser.runtime.onMessage.addListener(async (request) => {
+  const settings = await browser.storage.local.get('foreground');
+  browser.tabs.create({ url: request.href, active: !!settings.foreground });
 });
--- options.js
+++ options.js
@@ -1,18 +1,20 @@
 function saveOptions(e) {
-    e.preventDefault();
-    browser.storage.local.set({
-        foreground: document.querySelector("#foreground").checked,
-        suppressConfirm: document.querySelector("#suppress-confirm").checked,
-        markRead: document.querySelector("#mark-read").checked
-    });
+  e.preventDefault();
+  browser.storage.local.set({
+    foreground: document.querySelector('#foreground').checked,
+    suppressConfirm: document.querySelector('#suppress-confirm').checked,
+    markRead: document.querySelector('#mark-read').checked,
+    openSaved: document.querySelector('#open-saved').checked,
+  });
 }
 
 async function restoreOptions() {
-    let storage = await browser.storage.local.get();
-    document.querySelector("#foreground").checked = storage.foreground;
-    document.querySelector("#suppress-confirm").checked = storage.suppressConfirm;
-    document.querySelector("#mark-read").checked = storage.markRead;
+  const storage = await browser.storage.local.get();
+  document.querySelector('#foreground').checked = storage.foreground;
+  document.querySelector('#suppress-confirm').checked = storage.suppressConfirm;
+  document.querySelector('#mark-read').checked = storage.markRead;
+  document.querySelector('#open-saved').checked = storage.openSaved;
 }
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
